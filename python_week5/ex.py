a544aa17604eaa05b2c06f894a7277bf9923e922
tanchanok-tnk <67319010046@tnk.ac.th>
2024-11-17 14:51:43 +0700

    tanchanok

---
+++ python_week5/ex.py
@@ -0,0 +1,4 @@
+def funcion(a,b)
+for i in range(a+1):
+        if i % 3 != 0 :
+            print(i)
---
+++ python_week5/forloop.py
@@ -0,0 +1,5 @@
+import ex
+a = int(input('ตัวเลข'))
+b = int(input('ตัวเลข'))
+d = ex.function(1,10)
+print(d)
